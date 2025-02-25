d1644eb500f98add06f6092e0c8437140122cbce
parazyd <parazyd@dyne.org>
2025-02-25 12:09:55 +0100

    url/parser: Add additional schemes

--- url/src/parser.rs
+++ url/src/parser.rs
@@ -176,7 +176,8 @@ impl SchemeType {
 impl<T: AsRef<str>> From<T> for SchemeType {
     fn from(s: T) -> Self {
         match s.as_ref() {
-            "http" | "https" | "ws" | "wss" | "ftp" => Self::SpecialNotFile,
+            "tcp" | "tcp+tls" | "tor" | "tor+tls" | "socks5" | "socks5+tls" | "nym" | "nym+tls"
+            | "quic" | "http" | "https" | "ws" | "wss" | "ftp" => Self::SpecialNotFile,
             "file" => Self::File,
             _ => Self::NotSpecial,
         }
